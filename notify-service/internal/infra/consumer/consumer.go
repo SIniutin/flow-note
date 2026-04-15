10e7d7c8086a873a4aa1cd1ad6ec1cf1ae0020f7
SInuytin <slavainuytin1@gmail.com>
2026-04-15 06:59:50 +0300

    i daun 3

--- notify-service/internal/infra/consumer/consumer.go
+++ notify-service/internal/infra/consumer/consumer.go
@@ -2,12 +2,9 @@ package consumer
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
 	"github.com/flow-note/common/broker"
 	"github.com/flow-note/common/events"
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 // eventProcessor is the inbound port consumed by the AMQP adapter.
@@ -19,22 +16,16 @@ type eventProcessor interface {
 // It subscribes to a queue, deserialises domain events, and delegates to the
 // ProcessEvent use case.
 type Consumer struct {
-	broker    broker.Consumer
+	broker    broker.RabbitMQ
 	queue     string
 	processor eventProcessor
 }
 
-func New(b broker.Consumer, queue string, processor eventProcessor) *Consumer {
+func New(b broker.RabbitMQ, queue string, processor eventProcessor) *Consumer {
 	return &Consumer{broker: b, queue: queue, processor: processor}
 }
 
 // Run blocks until ctx is cancelled or the broker returns an error.
 func (c *Consumer) Run(ctx context.Context) error {
-	return c.broker.Consume(ctx, c.queue, func(ctx context.Context, d amqp.Delivery) error {
-		var envelope events.Envelope
-		if err := json.Unmarshal(d.Body, &envelope); err != nil {
-			return fmt.Errorf("malformed event body: %w", err)
-		}
-		return c.processor.Execute(ctx, envelope)
-	})
+	return nil
 }
